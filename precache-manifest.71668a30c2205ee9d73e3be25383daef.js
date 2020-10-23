f3c9f106d7f45fce5ef3800c79b16ae027f0992b
abkhale <abkhale.17@gmail.com>
2020-10-24 01:14:32 +0530

    Updates

---
+++ precache-manifest.71668a30c2205ee9d73e3be25383daef.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2c4d42c351f8efaef844f914bbd971",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "e2619d0ca192a49ee69f",
+    "url": "/memory-card/static/css/main.609d2616.chunk.css"
+  },
+  {
+    "revision": "e3dd992b9802beb2a84f",
+    "url": "/memory-card/static/js/2.480f3feb.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/memory-card/static/js/2.480f3feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2619d0ca192a49ee69f",
+    "url": "/memory-card/static/js/main.26b6664b.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  },
+  {
+    "revision": "57b49fa4843851dd361299c0fafc19aa",
+    "url": "/memory-card/static/media/Alica.57b49fa4.jpg"
+  },
+  {
+    "revision": "a4a27e360ef31bd8bd293055b0715c77",
+    "url": "/memory-card/static/media/Amber_Heard.a4a27e36.jpg"
+  },
+  {
+    "revision": "d3c164c8d29e74e503d216841dc22449",
+    "url": "/memory-card/static/media/Beer.d3c164c8.jpg"
+  },
+  {
+    "revision": "69a021466c86059a786b9b5976019e0d",
+    "url": "/memory-card/static/media/Coffee.69a02146.jpg"
+  },
+  {
+    "revision": "f3a90a63363dded4e6529cb96cdd0707",
+    "url": "/memory-card/static/media/Coffee_Marshmallow.f3a90a63.jpg"
+  },
+  {
+    "revision": "522180022893802d15dc48a622e7ec1d",
+    "url": "/memory-card/static/media/Crabs.52218002.jpg"
+  },
+  {
+    "revision": "ffd40627a2890a906615539583243eb9",
+    "url": "/memory-card/static/media/Drinks_Hamburger.ffd40627.jpg"
+  },
+  {
+    "revision": "4b916497b709c4dd28fe1375240dbfda",
+    "url": "/memory-card/static/media/Eiza_Gonzalez.4b916497.jpg"
+  },
+  {
+    "revision": "8ad3c21a2ad5cfee86849900f7e7d1ce",
+    "url": "/memory-card/static/media/Jason_Bourne.8ad3c21a.jpg"
+  },
+  {
+    "revision": "7c634c69f92aefeb53457c55ab7a0710",
+    "url": "/memory-card/static/media/Jennifer_Lawrence.7c634c69.jpg"
+  },
+  {
+    "revision": "bd1d21821304f482d6481d8873fb3ce5",
+    "url": "/memory-card/static/media/Jimmy_McGill.bd1d2182.jpg"
+  },
+  {
+    "revision": "61f8bf6e15dedef1a26fa9f6135d59ed",
+    "url": "/memory-card/static/media/Juice.61f8bf6e.jpg"
+  },
+  {
+    "revision": "f4b95c683b0eb5167e33868ea5b7d693",
+    "url": "/memory-card/static/media/Lee_Ji-eun.f4b95c68.jpg"
+  },
+  {
+    "revision": "017f19560c2123484465e024136e8577",
+    "url": "/memory-card/static/media/Margot_Robbie.017f1956.jpg"
+  },
+  {
+    "revision": "1b99e51817c48a85b9bd5481e26af981",
+    "url": "/memory-card/static/media/Olga_Kurylenko.1b99e518.jpg"
+  },
+  {
+    "revision": "13a4ab89aa515a7c95dc14f58a6cceee",
+    "url": "/memory-card/static/media/Pancake.13a4ab89.jpg"
+  },
+  {
+    "revision": "8e2485aaf6d4eddbcc0c873c200d64fc",
+    "url": "/memory-card/static/media/Pastry_Pie.8e2485aa.jpg"
+  },
+  {
+    "revision": "95052a940fae3de471a2f0d57ab6ada6",
+    "url": "/memory-card/static/media/Pizza.95052a94.jpg"
+  },
+  {
+    "revision": "9e69d05a2311fa4e9e7ca27dcc9b1f05",
+    "url": "/memory-card/static/media/Rakshitha.9e69d05a.jpg"
+  },
+  {
+    "revision": "ce8781fd7cb5593ae0df3ee99ea9d113",
+    "url": "/memory-card/static/media/Scarlett_Johansson.ce8781fd.jpg"
+  },
+  {
+    "revision": "ac52e4c57074f976dd96ee8fefb29d74",
+    "url": "/memory-card/static/media/Strawberry.ac52e4c5.jpg"
+  },
+  {
+    "revision": "19e061972f220ddfa039d462ad0c9528",
+    "url": "/memory-card/static/media/Sushi.19e06197.jpg"
+  },
+  {
+    "revision": "41e6180e6c54a3de6d9bd80e1bbf499e",
+    "url": "/memory-card/static/media/Tom_Hardy.41e6180e.jpg"
+  },
+  {
+    "revision": "925b2d3b0a74736eff9bbcd96f6af463",
+    "url": "/memory-card/static/media/pasta.925b2d3b.jpg"
+  }
+]);
